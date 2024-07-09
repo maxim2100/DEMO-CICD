41751ecc9dc3052bf7a7ae0ad95c7cd155919fdd
maxim2100 <maxsim2100@gmail.com>
2024-07-09 15:25:07 +0300

    test update

--- tests/Array.spec.js
+++ tests/Array.spec.js
@@ -1,11 +1,10 @@
 import { expect } from "chai";
 
-
 describe("stam test suite", () => {
     describe("Test array get sorted", () => {
         it("should sort array by name", () => {
-            const name = ["a", "c", "v","b","a"];
-            expect(name.sort()).to.equal(['a', 'a', 'b', 'c', 'v' ]);
-        })
-    })
-})
+            const names = ["a", "c", "v", "b", "a"];
+            expect(names.sort()).to.deep.equal(['a', 'a', 'b', 'c', 'v']);
+        });
+    });
+});
